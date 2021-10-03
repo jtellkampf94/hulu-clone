114e0b61870bd9217a8de2e11093e8cf76af2941
Jonathan <jtellkampf@gmail.com>
2021-10-03 21:41:47 +0100

    Completed styles of thumbnail

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 const Thumbnail = ({ result }) => {
   const BASE_URL = `https://image.tmdb.org/t/p/original/`;
   return (
-    <div className="group cursor-pointer">
+    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -16,7 +16,9 @@ const Thumbnail = ({ result }) => {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="">{result.title || result.original_name}</h2>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.original_name}
+        </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type.toUpperCase()} `}&#183;{" "}
           {result.release_date || result.first_air_date} &#183;{" "}
